0f233b6b8154ea68d10f7d1ef28e2357a4991897
Monica Marziano <mmarziano1@gmail.com>
2019-07-14 23:00:15 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190714224339) do
+ActiveRecord::Schema.define(version: 20140212205318) do
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "passengers_taxis", id: false, force: :cascade do |t|
-    t.integer "taxi_id",      null: false
-    t.integer "passenger_id", null: false
-    t.index ["passenger_id"], name: "index_passengers_taxis_on_passenger_id"
-    t.index ["taxi_id"], name: "index_passengers_taxis_on_taxi_id"
   end
 
   create_table "rides", force: :cascade do |t|
