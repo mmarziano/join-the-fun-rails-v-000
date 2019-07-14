52cab37f265fa68af25c8076f1adf5a845cd810e
Monica Marziano <mmarziano1@gmail.com>
2019-07-14 22:45:14 +0000

    Automatically backed up by Learn

--- db/migrate/20190714224339_create_join_table.rb
+++ db/migrate/20190714224339_create_join_table.rb
@@ -1,7 +1,9 @@
 class CreateJoinTable < ActiveRecord::Migration[5.0]
   def change
-    create_join_table :, : do |t|
+    create_join_table :taxis, :passengers do |t|
       # t.index [:_id, :_id]
+      t.index :taxi_id
+      t.index :passenger_id
       # t.index [:_id, :_id]
     end
   end
